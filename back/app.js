8f2fae764dfdf09d1d5b1d040069a533a54eff86
Kim-Yun-Seo <rlagrace410@gmail.com>
2023-08-24 17:07:10 +0900

    feat: back folder 재설정

--- back/app.js
+++ back/app.js
@@ -2,8 +2,15 @@ const express = require('express')();
 const app = express;
 const port = 9001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+app.get('/', (req, res) => {
+  res.send("hello world!!!!!!!!!~!!!!!")
+})
+
+app.get('/api/account', (req, res) => {
+  res.send({
+    mid: 3,
+    memeberName: "홍길동"
+  })
 })
 
 app.listen(port, () => {
