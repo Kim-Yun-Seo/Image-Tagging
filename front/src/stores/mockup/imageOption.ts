f7756d40bf46481b4f26595782eab1eff39fa7e6
Kim-Yun-Seo <rlagrace410@gmail.com>
2023-08-31 11:13:07 +0900

    add: mockup 설정

--- front/src/stores/mockup/imageOption.ts
+++ front/src/stores/mockup/imageOption.ts
@@ -1,10 +1,69 @@
-export default () => {
-  const imageInfo = {
-    img1: {
-      url: '',
-      tag: ['a boy', 'smile', 'masterpiece']
-    }
+import { reactive, ref } from 'vue'
+export const state = reactive({
+  account: {
+    id: null,
+    name: '',
+    url: ''
+  },
+  form: {
+    loginId: '',
+    loginPw: '',
+    url: ''
   }
+})
 
-  return imageInfo
-}
+export const upload = reactive({
+  form: {
+    img: ''
+  }
+})
+
+export const imageInfo = ref(
+  {
+    files: {
+      1: {
+        key: '1',
+        src: 'https://cdn.imweb.me/thumbnail/20211229/a85203b810b76.jpg',
+        tags: ['a girl', 'tiara', 'good']
+      },
+      2: {
+        key: '2',
+        src: 'https://i1.sndcdn.com/avatars-xsoyM023yLDj8S6B-DCiezQ-t500x500.jpg',
+        tags: ['a girl', 'surprise', 'masterpiece']
+
+      },
+      3: {
+        key: '3',
+        src: 'https://i.pinimg.com/originals/f7/77/55/f7775543e15e04b22fc7008bae1e5899.jpg',
+        tags: ['a girl', 'angry', 'braided hair']
+      },
+      4: {
+        key: '4',
+        src: 'https://itimg.chosun.com/sitedata/image/201807/06/2018070602943_0.jpg',
+        tags: ['a girl', 'confused', 'pan']
+      },
+      5: {
+        key: '5',
+        src: 'https://i.pinimg.com/736x/60/69/86/606986d82fa7f4801076673eb661b4f5.jpg',
+        tags: ['a baby', 'shy', 'big eyes']
+      },
+      6: {
+        key: '6',
+        src: 'https://i.namu.wiki/i/V4lnpG0NHcsRUTVPMFil1z6YPv3b0d4_fCrsVId48o10EE6QTQBMP3jVGdRxNdbUlspPIuign7q52yoT5FGTPw.webp',
+        tags: ['a girl', 'confuesd', 'short brown hair']
+      },
+      7: {
+        key: '7',
+        src: 'https://image.jtbcplus.kr/data/contents/jam_photo/202003/30/fb002e68-7a2d-4317-8418-6bc12fde5e71.jpg',
+        tags: ['a girl', 'blonde', 'singing']
+      },
+      8: {
+        key: '8',
+        src: 'https://post-phinf.pstatic.net/MjAxOTA1MjFfMjIg/MDAxNTU4NDQ3MzcyOTk5.RAPmZq6iI5szHe6Kdl6G5JCSD_k_Ib3qd_A3GjdTOMkg.P6Irrxfwnfs04q3PLzp4CeLmaFfgLOXTmaOmvynPfE0g.JPEG/%EB%9D%BC%ED%91%BC%EC%A0%A4.jpg?type=w800_q75',
+        tags: ['a girl', 'pet', 'pascal']
+      }
+    },
+    selected: '',
+    filesTags: ''
+  }
+)
